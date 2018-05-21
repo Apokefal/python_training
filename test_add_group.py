97a67583d6a41359d111c5d0be24ed044de86b56
Walerko <apokefal89@gmail.com>
2018-05-21 15:11:02 +0700

    Записан рекордем сценарий Сценарий отредактирован добавлен автотест с созданием группы с пустым названием

--- test_add_group.py
+++ test_add_group.py
@@ -18,9 +18,19 @@ class test_add_group(unittest.TestCase):
         success = True
         wd = self.wd
         self.Open_home_page(wd)
-        self.Login(wd)
+        self.Login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd)
+        self.create_group(wd, name="apo", header="apok", footer="apokefa")
+        self.Return_to_group_page(wd)
+        self.Logout(wd)
+
+    def test_test_add_empty_group(self):
+        success = True
+        wd = self.wd
+        self.Open_home_page(wd)
+        self.Login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, name="", header="", footer="")
         self.Return_to_group_page(wd)
         self.Logout(wd)
 
@@ -30,32 +40,32 @@ class test_add_group(unittest.TestCase):
     def Return_to_group_page(self, wd):
         wd.find_element_by_link_text("group page").click()
 
-    def create_group(self, wd):
+    def create_group(self, wd, name, header, footer):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("apo")
+        wd.find_element_by_name("group_name").send_keys(name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("apok")
+        wd.find_element_by_name("group_header").send_keys(header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("apokefa")
+        wd.find_element_by_name("group_footer").send_keys(footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
 
     def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
 
-    def Login(self, wd):
+    def Login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
     def Open_home_page(self, wd):
