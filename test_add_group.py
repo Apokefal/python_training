76eaa48abb5e41518002bd4c27438daf2671fcd6
Walerko <apokefal89@gmail.com>
2018-05-21 15:46:48 +0700

    Записан рекордем сценарий Сценарий отредактирован добавлен автотест с созданием группы с пустым названием Автотест модернизирован, добавлены еще 2 автотеста

--- test_add_group.py
+++ test_add_group.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
+from group import Group
 
 def is_alert_present(wd):
     try:
@@ -20,7 +21,7 @@ class test_add_group(unittest.TestCase):
         self.Open_home_page(wd)
         self.Login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd, name="apo", header="apok", footer="apokefa")
+        self.create_group(wd, Group(name="apo", header="apok", footer="apokefa"))
         self.Return_to_group_page(wd)
         self.Logout(wd)
 
@@ -30,7 +31,17 @@ class test_add_group(unittest.TestCase):
         self.Open_home_page(wd)
         self.Login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd, name="", header="", footer="")
+        self.create_group(wd, Group(name="", header="", footer=""))
+        self.Return_to_group_page(wd)
+        self.Logout(wd)
+
+    def test_test_add_incorrect_group(self):
+        success = True
+        wd = self.wd
+        self.Open_home_page(wd)
+        self.Login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, Group(name="%", header="%", footer="%"))
         self.Return_to_group_page(wd)
         self.Logout(wd)
 
@@ -40,19 +51,19 @@ class test_add_group(unittest.TestCase):
     def Return_to_group_page(self, wd):
         wd.find_element_by_link_text("group page").click()
 
-    def create_group(self, wd, name, header, footer):
+    def create_group(self, wd, group):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(name)
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(header)
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(footer)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
 
