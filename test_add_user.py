c108fb80130d9943daa7ee185714bd2b601b6e99
Walerko <apokefal89@gmail.com>
2018-05-28 17:48:00 +0700

    Модернизация теста на создание юзера создание fixture

--- test_add_user.py
+++ test_add_user.py
@@ -1,153 +1,20 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
+import pytest
 from userform import UsFo
+from contr import Contr
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+@pytest.fixture()
+def ctr(request):
+    fixture = Contr()
+    request.addfinalizer(fixture.murder)
+    return fixture
 
-class test_add_user(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver(capabilities={"marionette": False})
-        self.wd.implicitly_wait(60)
-    
-    def test_test_add_user(self):
-        success = True
-        wd = self.wd
-        self.Open_home_page(wd)
-        self.Login(wd, username="admin", password="secret")
-        self.Add_user(wd, UsFo(firstname="привет", middlename="привет", lastname="привет", nickname="привет", title="привет", company="привет", address="привет", home="привет", mobile="привет", work="привет", fax="привет", email="привет", email2="привет", email3="привет", homepage="привет", yearbirt="1989", yearanni="1989", address2="привет", phone2="привет", notes="привет"))
-        self.Return_home_page(wd)
-        self.Logout(wd)
-        self.assertTrue(success)
+def test_test_add_user(ctr):
+    ctr.Login(username="admin", password="secret")
+    ctr.Add_user(UsFo(firstname="привет", middlename="привет", lastname="привет", nickname="привет", title="привет", company="привет", address="привет", home="привет", mobile="привет", work="привет", fax="привет", email="привет", email2="привет", email3="привет", homepage="привет", yearbirt="1989", yearanni="1989", address2="привет", phone2="привет", notes="привет"))
+    ctr.Logout()
 
-    def test_test_add_empty_user(self):
-        success = True
-        wd = self.wd
-        self.Open_home_page(wd)
-        self.Login(wd, username="admin", password="secret")
-        self.Add_user(wd, UsFo(firstname="", middlename="", lastname="", nickname="", title="", company="", address="", home="", mobile="", work="", fax="", email="", email2="", email3="", homepage="", yearbirt="", yearanni="", address2="", phone2="", notes=""))
-        self.Return_home_page(wd)
-        self.Logout(wd)
-        self.assertTrue(success)
-
-    def Logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def Return_home_page(self, wd):
-        wd.find_element_by_link_text("home page").click()
-
-    def Add_user(self, wd, userform):
-        # init user creation
-        wd.find_element_by_link_text("add new").click()
-        # Firstname
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(userform.firstname)
-        # Middlename
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(userform.middlename)
-        # Lastname
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(userform.lastname)
-        # Nickname
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(userform.nickname)
-        # Title
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(userform.title)
-        # Company
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(userform.company)
-        # Address
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(userform.address)
-        # Telephone home
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(userform.home)
-        # Telephone mobile
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(userform.mobile)
-        # Telephone work
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(userform.work)
-        # Telephone fax
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(userform.fax)
-        # E-mail
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(userform.email)
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(userform.email2)
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(userform.email3)
-        # Homepage
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(userform.homepage)
-        # Birthday
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[3]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[3]").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[4]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[4]").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(userform.yearbirt)
-        # Anniversary
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[4]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[4]").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[5]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[5]").click()
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(userform.yearanni)
-        # Secondary address
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(userform.address2)
-        # Secondary home
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(userform.phone2)
-        # Secondary notes
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(userform.notes)
-        # Submit group creation
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def Login(self, wd, username, password):
-        # Login
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def Open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/")
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
+def test_test_add_empty_user(ctr):
+    ctr.Login(username="admin", password="secret")
+    ctr.Add_user(UsFo(firstname="", middlename="", lastname="", nickname="", title="", company="", address="", home="", mobile="", work="", fax="", email="", email2="", email3="", homepage="", yearbirt="", yearanni="", address2="", phone2="", notes=""))
+    ctr.Logout()
