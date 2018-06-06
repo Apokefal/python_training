5d6c58114b58a7364220ba53a3102582e401dfcf
Валери <wandkate@gmail.com>
2018-06-06 23:49:58 +0700

    Повысил интеллект фикстуры

--- conftest.py
+++ conftest.py
@@ -12,14 +12,14 @@ def app(request):
     else:
         if not fixture.is_valid():
             fixture = Application()
-            fixture.session.Login(username="admin", password="secret")
+    fixture.session.ensure_Login(username="admin", password="secret")
     return fixture
 
 
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
     def fin():
-        fixture.session.Logout()
+        fixture.session.ensure_Logout()
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,9 +5,9 @@ from model.Data import Group
 def test_test_add_group(app):
     app.group.create(Group(name="apo", header="apok", footer="apokefa"))
 
-
 def test_test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
 
 def test_test_add_incorrect_group(app):
     app.group.create(Group(name="%", header="%", footer="%"))
+
