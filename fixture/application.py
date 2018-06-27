85e6b9930fb8c654ab4def3e0b30ea2270673cc1
Apokefal <Apokefal89@gmail.com>
2018-06-27 18:48:44 +0700

    Ощибка

--- fixture/application.py
+++ fixture/application.py
@@ -1,16 +1,24 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
+#from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.user import UserHelper
 ###
 
 class Application:
-    def __init__(self):
-        self.wd = WebDriver(capabilities={"marionette": False})
-        self.wd.implicitly_wait(1)
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox(capabilities={"marionette": False})
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized  browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.user = UserHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -21,7 +29,9 @@ class Application:
 
     def Open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        if not (wd.current_url.endswith("http://localhost/addressbook/") and
+                len(wd.find_elements_by_link_text("add new")) > 0):
+            wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
